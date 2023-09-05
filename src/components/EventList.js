611fabb798b7885c894737ac2b8a7d51c3e3619e
hansashi <ahantke@mail.de>
2023-09-05 21:03:39 +0200

    added events test

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,8 +1,14 @@
 // src/components/EventList.js
 
-const EventList = () => {
+import Event from "./Event";
+
+const EventList = ({ events }) => {
   return (
-    <ul id="event-list"></ul>
+    <ul id="event-list">
+      {events ?
+        events.map(event => <Event key={event.id} event={event} />) :
+        null}
+    </ul>
   );
 }
 
