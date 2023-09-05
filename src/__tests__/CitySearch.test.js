cf08df9ae87b4e0956dfb71c24480c5035d16de4
hansashi <ahantke@mail.de>
2023-09-05 22:35:26 +0200

    implemented code for CitySearch test

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -3,6 +3,7 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
+import { extractLocations, getEvents } from '../api';
 
 describe('<CitySearch /> component', () => {
   let CitySearchComponent;
