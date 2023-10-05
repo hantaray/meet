de4596a12d799a1bf479b5f12de526a24defb373
hansashi <ahantke@mail.de>
2023-10-05 20:05:03 +0200

    added Alert component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { getEvents, extractLocations } from './api';
+import { InfoAlert } from './components/Alert';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -31,7 +33,14 @@ function App() {
     <div className="App">
       <h1>Show events in your city</h1>
       <label>Enter/select city:</label>
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+      </div>
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+      />
 
       <div className="test">
         <label>Enter/select visible events:</label>
