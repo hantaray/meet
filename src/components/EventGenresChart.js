00bf4d8c3867607257207acfd37578f2260832f6
hansashi <ahantke@mail.de>
2023-10-07 20:38:36 +0200

    added CustomizedLabel

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -22,12 +22,23 @@ const EventGenresChart = ({ events }) => {
     return data;
   };
 
-  // const data = [
-  //   { name: "Group A", value: 500 },
-  //   { name: "Group B", value: 500 },
-  //   { name: "Group C", value: 300 },
-  //   { name: "Group D", value: 200 }
-  // ];
+  const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
 
   return (
     <ResponsiveContainer width="99%" height={400}>
@@ -37,7 +48,7 @@ const EventGenresChart = ({ events }) => {
           dataKey="value"
           fill="#8884d8"
           labelLine={false}
-          label
+          label={renderCustomizedLabel}
           outerRadius={130}
         />
       </PieChart>
