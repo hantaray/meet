f0cc176a10410c360d7fba3c291288f664c8a032
hansashi <ahantke@mail.de>
2023-10-07 15:36:19 +0200

    removed NProgress

--- src/api.js
+++ src/api.js
@@ -46,7 +46,6 @@ export const getEvents = async () => {
 
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
-    NProgress.done();
     return events ? JSON.parse(events) : [];
   }
 
@@ -58,7 +57,6 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
