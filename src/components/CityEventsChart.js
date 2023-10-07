416e2453d4588ef7384098bb7b5a2b8566d7966a
hansashi <ahantke@mail.de>
2023-10-07 19:49:27 +0200

    updated chart

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -32,7 +32,7 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 120,
+          bottom: 100,
           left: -30,
         }}
       >
